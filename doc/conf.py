8085f6a7103d7bce692aba32a6387558be429fe4
Sam Avis <avissam96@gmail.com>
2021-04-20 17:11:33 +0100

    DOC: Added logo

--- doc/conf.py
+++ doc/conf.py
@@ -58,6 +58,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Logo
+html_logo = "logo/logo2.png"
+html_theme_options = {'logo_only': True}
+
 
 # -- Options for breathe -----------------------------------------------------
 
