50bfcbd4ea6f7171b36e15171e063e92c47fab6d
Sam Avis <avissam96@gmail.com>
2022-09-21 22:54:43 +0100

    VEC: Removed from minim namespace

--- include/utils/vec.h
+++ include/utils/vec.h
@@ -3,37 +3,34 @@
 
 #include <vector>
 
-namespace minim {
-  typedef std::vector<double> Vector;
 
-  Vector operator+(double a, Vector b);
-  Vector operator+(const Vector &a, double b);
-  Vector operator+(const Vector &a, const Vector &b);
-  Vector& operator+=(Vector &a, double b);
-  Vector& operator+=(Vector &a, const Vector &b);
+std::vector<double> operator+(double a, std::vector<double> b);
+std::vector<double> operator+(const std::vector<double> &a, double b);
+std::vector<double> operator+(const std::vector<double> &a, const std::vector<double> &b);
+std::vector<double>& operator+=(std::vector<double> &a, double b);
+std::vector<double>& operator+=(std::vector<double> &a, const std::vector<double> &b);
 
-  Vector operator-(const Vector &a);
-  Vector operator-(double a, const Vector &b);
-  Vector operator-(const Vector &a, double b);
-  Vector operator-(const Vector &a, const Vector &b);
-  Vector& operator-=(Vector &a, double b);
-  Vector& operator-=(Vector &a, const Vector &b);
+std::vector<double> operator-(const std::vector<double> &a);
+std::vector<double> operator-(double a, const std::vector<double> &b);
+std::vector<double> operator-(const std::vector<double> &a, double b);
+std::vector<double> operator-(const std::vector<double> &a, const std::vector<double> &b);
+std::vector<double>& operator-=(std::vector<double> &a, double b);
+std::vector<double>& operator-=(std::vector<double> &a, const std::vector<double> &b);
 
-  Vector operator*(double a, const Vector &b);
-  Vector operator*(const Vector &a, double b);
-  Vector& operator*=(Vector &a, double b);
+std::vector<double> operator*(double a, const std::vector<double> &b);
+std::vector<double> operator*(const std::vector<double> &a, double b);
+std::vector<double>& operator*=(std::vector<double> &a, double b);
 
-  namespace vec {
-    double dotProduct(const Vector &a, const Vector &b);
-    Vector crossProduct(const Vector &a, const Vector &b);
+namespace vec {
+  double dotProduct(const std::vector<double> &a, const std::vector<double> &b);
+  std::vector<double> crossProduct(const std::vector<double> &a, const std::vector<double> &b);
 
-    double norm(const Vector &a);
+  double norm(const std::vector<double> &a);
 
-    bool any(const std::vector<bool> &a);
-    bool all(const std::vector<bool> &a);
+  bool any(const std::vector<bool> &a);
+  bool all(const std::vector<bool> &a);
 
-    void insert_unique(std::vector<int> &vec, int value);
-  }
+  void insert_unique(std::vector<int> &vec, int value);
 }
 
 #endif
