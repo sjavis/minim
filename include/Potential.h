b1051169b2bd5e65df4390ff6f93373070b1258d
Sam Avis <avissam96@gmail.com>
2023-05-03 09:39:20 +0100

    POT: Add constraints to header and reorder header

--- include/Potential.h
+++ include/Potential.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <memory>
+#include <functional>
 
 namespace minim {
   class State;
@@ -21,6 +22,26 @@ namespace minim {
     EGFunc _energyGradient;
 
     public:
+      // Basic user functions
+      Potential(EFunc energy, GFunc gradient);
+      Potential(EGFunc energyGradient);
+
+      State newState(int ndof, const vector<int>& ranks={});
+      virtual State newState(const vector<double>& coords, const vector<int>& ranks={});
+
+      virtual double energy(const vector<double>& coords) const;
+      virtual vector<double> gradient(const vector<double>& coords) const;
+      virtual void energyGradient(const vector<double>& coords, double* e, vector<double>* g) const;
+
+      // Initialisation
+      bool serialDef() const;
+      bool parallelDef() const;
+
+      virtual void init(const vector<double>& coords) {};
+      virtual void distributeParameters(const Communicator& comm) {}; // Take care using this, if the potential is cloned any distributed parameters will be copied as they are
+      bool distributed = false;
+
+      // Energy elements for parallelisation
       struct Element {
         int type;
         vector<int> idof;
@@ -28,37 +49,32 @@ namespace minim {
       };
       vector<Element> elements;
       vector<Element> elements_halo;
-      bool distributed = false;
-
-
-      Potential(EFunc energy, GFunc gradient);
-      Potential(EGFunc energyGradient);
-      ~Potential() {};
-      virtual std::unique_ptr<Potential> clone() const {
-        return std::make_unique<Potential>(*this);
-      }
-
-      virtual void init(const vector<double>& coords) {};
-      virtual void distributeParameters(const Communicator& comm) {}; // Take care using this, if the potential is cloned any distributed parameters will be copied as they are
-
-
-      virtual double energy(const vector<double>& coords) const;
-      virtual vector<double> gradient(const vector<double>& coords) const;
-      virtual void energyGradient(const vector<double>& coords, double* e, vector<double>* g) const;
-      virtual void elementEnergyGradient(const vector<double>& coords, const Element& el, double* e, vector<double>* g) const;
-      virtual void blockEnergyGradient(const vector<double>& coords, const Communicator& comm, double* e, vector<double>* g) const {};
-
-      bool serialDef() const;
-      bool parallelDef() const;
-
-
-      State newState(int ndof, const vector<int>& ranks={});
-      virtual State newState(const vector<double>& coords, const vector<int>& ranks={});
-
 
       Potential& setElements(vector<Element> elements);
       Potential& setElements(vector2d<int> idofs);
       Potential& setElements(vector2d<int> idofs, vector<int> types, vector2d<double> parameters);
+
+      virtual void elementEnergyGradient(const vector<double>& coords, const Element& el, double* e, vector<double>* g) const;
+      virtual void blockEnergyGradient(const vector<double>& coords, const Communicator& comm, double* e, vector<double>* g) const {};
+
+      // Constraints
+      struct Constraint {
+        vector<int> idof;
+        std::function<vector<double>(const vector<double>&)> normal;
+        std::function<void(vector<double>&)> correction = nullptr;
+      };
+      vector<Constraint> constraints;
+
+      Potential& setConstraints(vector<Constraint> constraints);
+      Potential& setConstraints(vector<int> iFix);
+      Potential& setConstraints(vector2d<int> idofs, vector<double> normal, std::function<void(vector<double>&)> correction=nullptr);
+      Potential& setConstraints(vector2d<int> idofs, std::function<vector<double>(const vector<double>&)> normal, std::function<void(vector<double>&)> correction=nullptr);
+
+      // Copy / destruct
+      ~Potential() {};
+      virtual std::unique_ptr<Potential> clone() const {
+        return std::make_unique<Potential>(*this);
+      }
 
     protected:
       Potential() : _energy(nullptr), _gradient(nullptr), _energyGradient(nullptr) {};
@@ -85,6 +101,19 @@ namespace minim {
       }
       Derived& setElements(vector2d<int> idofs, vector<int> types, vector2d<double> parameters) {
         return static_cast<Derived&>(Potential::setElements(idofs, types, parameters));
+      }
+
+      Derived& setConstraints(vector<Constraint> constraints) {
+        return static_cast<Derived&>(Potential::setConstraints(constraints));
+      }
+      Derived& setConstraints(vector<int> iFix) {
+        return static_cast<Derived&>(Potential::setConstraints(iFix));
+      }
+      Derived& setConstraints(vector2d<int> idofs, vector<double> normal, std::function<void(vector<double>&)> correction=nullptr) {
+        return static_cast<Derived&>(Potential::setConstraints(idofs, normal, correction));
+      }
+      Derived& setConstraints(vector2d<int> idofs, std::function<vector<double>(const vector<double>&)> normal, std::function<void(vector<double>&)> correction=nullptr) {
+        return static_cast<Derived&>(Potential::setConstraints(idofs, normal, correction));
       }
   };
 
