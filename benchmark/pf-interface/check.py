b064378959aa905b5915118f8356b7da99ae0f95
Sam Avis <avissam96@gmail.com>
2024-05-22 15:42:22 +0100

    Added benchmark folder and pf-interface benchmark

---
+++ benchmark/pf-interface/check.py
@@ -0,0 +1,37 @@
+import sys
+import numpy as np
+from scipy.optimize import curve_fit
+
+plot = True if (len(sys.argv)!=2) else bool(int(sys.argv[1]))
+
+
+def interface(x, x0, width):
+    return 0.5 + 0.5*np.tanh((x-x0)/(2*width))
+
+def fit_interface(phi):
+    x = np.arange(len(phi))
+    [x0, width], _ = curve_fit(interface, x, phi, [len(phi)/2, 1])
+    return x0, width
+
+
+widths = np.array([0.25, 0.5, 1, 2, 4])
+errors = np.zeros(len(widths))
+for i_w, w0 in enumerate(widths):
+    data = np.loadtxt(f"width-{w0:g}.txt")
+    data = data.reshape((-1, 2))
+
+    phi = data[:,0]
+    x0, width = fit_interface(phi)
+    errors[i_w] = np.abs(width - w0) / w0
+
+
+if (plot):
+    import matplotlib.pyplot as plt
+    plt.plot(widths, errors, 'ks-')
+    plt.loglog()
+    plt.xlabel("Interface width")
+    plt.ylabel("Error in measured width")
+    plt.show()
+
+if (errors[widths==1] > 0.025):
+    sys.exit(1)
---
+++ benchmark/pf-interface/main.cpp
@@ -0,0 +1,72 @@
+#include "minim.h"
+#include "utils/vec.h"
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+
+using namespace minim;
+using std::vector;
+
+int nx = 60;
+
+
+vector<double> initialiseFluid() {
+  vector<double> data(2*nx);
+  for (int x=0; x<nx; x++) {
+    if (x > (nx-1)/2.0) {
+      data[2*x+0] = 1;
+    } else {
+      data[2*x+1] = 1;
+    }
+  }
+  return data;
+}
+
+
+void output(vector<double> data, std::string filename) {
+  if (mpi.rank == 0) {
+    std::ofstream f(filename);
+    for (auto x: data) {
+      f << x << std::endl;
+    }
+    f.close();
+  }
+}
+
+
+void run(double width) {
+  print("Width:", width);
+  double surfaceTension = 1e4;
+  double resolution = 1e-8;
+
+  PhaseField pf;
+  pf.setNFluid(2);
+  pf.setGridSize({nx, 1, 1});
+  pf.setResolution(resolution);
+  pf.setSurfaceTension(surfaceTension);
+  pf.setInterfaceSize(width*resolution);
+  pf.setSolid([](int x, int y, int z){ return (x==0 || x==nx-1);});
+
+  State state(pf, initialiseFluid());
+  state.convergence = 1e-15*surfaceTension*resolution*resolution;
+
+  Lbfgs min;
+  min.setLinesearch("none");
+  min.setMaxIter(1000);
+  auto minimum = min.minimise(state);
+
+  std::string filename = (std::stringstream() << "width-" << width <<".txt").str();
+  output(minimum, filename);
+}
+
+
+int main(int argc, char** argv) {
+  mpi.init(&argc, &argv);
+
+  vector<double> widths = {0.25, 0.5, 1, 2, 4};
+  for (auto width: widths) {
+    run(width);
+  }
+
+  return 0;
+}
